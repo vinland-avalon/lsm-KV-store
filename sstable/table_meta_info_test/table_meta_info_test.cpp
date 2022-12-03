b7493b220b157b0be8fc11260c4581508949de28
Bohan Wu <819186192@qq.com>
2022-12-03 22:00:52 +0800

    format tests for ss_table

--- sstable/table_meta_info_test/table_meta_info_test.cpp
+++ sstable/table_meta_info_test/table_meta_info_test.cpp
@@ -2,16 +2,16 @@
  * @Author: BohanWu 819186192@qq.com
  * @Date: 2022-11-30 23:02:04
  * @LastEditors: BohanWu 819186192@qq.com
- * @LastEditTime: 2022-12-01 01:40:44
- * @FilePath: /lsm-KV-store/sstable/table_meta_info_test.cpp
+ * @LastEditTime: 2022-12-03 21:57:25
+ * @FilePath: /lsm-KV-store/sstable/table_meta_info_test/table_meta_info_test.cpp
  * @Description:
  *
  * Copyright (c) 2022 by BohanWu 819186192@qq.com, All Rights Reserved.
  */
-#include "./table_meta_info.cpp"
+#include "../table_meta_info.cpp"
 #include <iostream>
 
-// g++ -o table_meta_info_test ./table_meta_info_test.cpp ./table_meta_info.cpp
+// g++ -o table_meta_info_test ./table_meta_info_test.cpp ../table_meta_info.cpp
 // ./table_meta_info_test
 int main() {
     TableMetaInfo *info = new TableMetaInfo();
@@ -22,12 +22,12 @@ int main() {
     info->setPartitionSize(5);
 
     std::fstream file;
-    file.open("./test_ssd", std::ios::binary | std::ios::out);
+    file.open("./ssd_for_table_meta_info_test", std::ios::binary | std::ios::out);
     file.write("iambohanwuandyou?", 15);
     info->writeToFile(&file);
     file.close();
 
-    file.open("./test_ssd", std::ios::binary | std::ios::in);
+    file.open("./ssd_for_table_meta_info_test", std::ios::binary | std::ios::in);
     TableMetaInfo *info2 = new TableMetaInfo();
     info2->readFromFile(&file);
     std::cout << info2->getDataLen() << '\t' << info2->getDataStart() << std::endl;

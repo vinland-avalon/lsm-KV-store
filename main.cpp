c37b18ae1c694d35109bed23da1d04f017d8cfc5
Bohan Wu <819186192@qq.com>
2022-12-08 15:38:09 +0800

    add CMakeLists.txt to src

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
  * @Author: BohanWu 819186192@qq.com
  * @Date: 2022-12-07 18:04:21
  * @LastEditors: BohanWu 819186192@qq.com
- * @LastEditTime: 2022-12-07 19:14:58
+ * @LastEditTime: 2022-12-08 15:34:24
  * @FilePath: /lsm-KV-store/main.cpp
  * @Description:
  *
  * Copyright (c) 2022 by BohanWu 819186192@qq.com, All Rights Reserved.
  */
+#include "./src/db.cpp"
+#include "./src/utils_for_time_operation.h"
 #include <iostream>
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
@@ -15,5 +17,7 @@ using json = nlohmann::json;
 int main(int, char **) {
     json j;
     j["hello"] = "world";
-    std::cout << "j['Hello'] = " << j["hello"] << std::endl;
+    // std::cout << "in ./src/utils_for_time_operation.h, getSystemTimeInMills:" << getSystemTimeInMills()
+    //           << ", j['Hello'] = " << j["hello"] << std::endl;
+    say_hello();
 }

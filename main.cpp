3cf90299159e628967d26e9138a1228b7b724d05
Bohan Wu <819186192@qq.com>
2022-12-09 11:17:19 +0800

    automatic build and debug: by launch.json and tasks.json

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
  * @Author: BohanWu 819186192@qq.com
  * @Date: 2022-12-07 18:04:21
  * @LastEditors: BohanWu 819186192@qq.com
- * @LastEditTime: 2022-12-08 20:57:00
+ * @LastEditTime: 2022-12-09 11:04:09
  * @FilePath: /lsm-KV-store/main.cpp
  * @Description:
  *
  * Copyright (c) 2022 by BohanWu 819186192@qq.com, All Rights Reserved.
  */
-#include "./db/utils_for_time_operation.h"
+#include "utils_for_time_operation.h"
 #include <iostream>
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
@@ -16,6 +16,8 @@ using json = nlohmann::json;
 int main(int, char **) {
     json j;
     j["hello"] = "world";
+    std::cout << "in ./src/utils_for_time_operation.h, getSystemTimeInMills:" << getSystemTimeInMills()
+              << ", j['Hello'] = " << j["hello"] << std::endl;
     std::cout << "in ./src/utils_for_time_operation.h, getSystemTimeInMills:" << getSystemTimeInMills()
               << ", j['Hello'] = " << j["hello"] << std::endl;
 }

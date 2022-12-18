4bc5df20f7c90f547e6cc92eb84815f5202cd47d
Bohan Wu <819186192@qq.com>
2022-12-18 22:41:17 +0800

    handle command of new structure

--- db/utils_for_json_operation.cpp
+++ db/utils_for_json_operation.cpp
@@ -7,15 +7,14 @@ using json = nlohmann::json;
 
 std::shared_ptr<Command> JSONtoCommand(json command_JSON) {
     std::shared_ptr<Command> command;
-    if (command_JSON["type"] == "SET") {
-        std::shared_ptr<std::string> ss = std::shared_ptr<std::string>(new std::string("AAA"));
-        command = std::shared_ptr<Command>(new SetCommand("SET", command_JSON["key"], command_JSON["value"]));
+    if (command_JSON[TypeField] == SetType) {
+        command = std::shared_ptr<Command>(InitSetCommand(command_JSON[KeyField], command_JSON[ValueField]));
     } else if (command_JSON["type"] == "RM") {
-        command = std::shared_ptr<Command>(new RmCommand("RM", command_JSON["key"]));
+        command = std::shared_ptr<Command>(InitRemoveCommand(command_JSON[KeyField]));
     } else {
         command = nullptr;
     }
     return command;
-    };
+};
 
 #endif
